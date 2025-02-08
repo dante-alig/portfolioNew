706190861b5b1dec360ad81308724f2c2506861a
dante-alig <samuel.celeste@gmail.com>
2025-02-08 23:30:14 +0100

    responsive warning

--- src/components/ScreenWarning.jsx
+++ src/components/ScreenWarning.jsx
@@ -4,11 +4,7 @@ const ScreenWarning = () => {
   return (
     <div className="screen-warning">
       <div className="warning-content">
-        <h2>responsive en construction</h2>
-        <p>
-          Pour une meilleure expérience, veuillez utiliser un écran plus petit
-          ou juste réduire la taille de votre fenêtre.
-        </p>
+        <p>Ce format n'est pas encore disponible</p>
       </div>
     </div>
   );
